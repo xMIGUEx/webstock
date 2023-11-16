1e345ebbaa260ae2bad245e788530bb86c7ac116
xMIGUEx <guuffyy@hotmail.com>
2023-11-15 19:58:39 -0500

    Entrada de base de datos
    Se creo la base de datos con exito y se esta procediendo a realizar el backend para la adicion, modificacion y eliminacion de datos de la base de datos desde la pagina web

---
+++ node_modules/todo-api/models/proveedores.model.js
@@ -0,0 +1,73 @@
+const execQuery = require('./../helpers/execQuery');
+const TYPES = require('tedious').TYPES;
+
+const addProveedor = (proveedorData) => {
+    const {
+        IDProveedor,
+        NombreProveedor,
+        DireccionProveedor,
+        NoTelefono,
+        Correo,
+        Web,
+        DescripcionProveedor,
+    } = proveedorData;
+    const query = `
+    INSERT INTO [dbo].[Proveedores] (IDProveedor, NombreProveedor, DireccionProveedor, NoTelefono, Correo, Web, DescripcionProveedor)
+    VALUES (@IDproveedor, @nombreproveedor,	@direccionproveedor, @notelefono, @correo, @web, @descripcionproveedor)
+    `;
+    const parameters = [
+        {name: 'IDProveedor', type: TYPES.Int, value: IDProveedor},
+        {name: 'NombreProveedor', type: TYPES.VarChar, value: NombreProveedor},
+        {name: 'DireccionProveedor', type: TYPES.VarChar, value: DireccionProveedor},
+        {name: 'NoTelefono', type: TYPES.VarChar, value: NoTelefono},
+        {name: 'Correo', type: TYPES.Int, VarChar: Correo},
+        {name: 'Web', type: TYPES.Int, VarChar: Web},
+        {name: 'DescripcionProveedor', type: TYPES.VarChar, value: DescripcionProveedor},
+    ];
+    return execQuery.execWriteCommand(query, parameters)
+};
+
+const updateProveedor = (proveedorData) => {
+    const {
+        IDProveedor,
+        NombreProveedor,
+        DireccionProveedor,
+        NoTelefono,
+        Correo,
+        Web,
+        DescripcionProveedor,
+    } = proveedorData;
+    const query = `
+    UPDATE [dbo].[Proveedores]
+    SET NombreProveedor=@nombreproveedor, DireccionProveedor=@direccionproveedor, NoTelefono=@notelefono, Correo=@correo, Web=@web, DescripcionProveedor=@descripcionproveedor
+    WHERE IDProveedor = @IDproveedor
+    `;
+    const parameters = [
+        {name: 'IDProveedor', type: TYPES.Int, value: IDProveedor},
+        {name: 'NombreProveedor', type: TYPES.VarChar, value: NombreProveedor},
+        {name: 'DireccionProveedor', type: TYPES.VarChar, value: DireccionProveedor},
+        {name: 'NoTelefono', type: TYPES.VarChar, value: NoTelefono},
+        {name: 'Correo', type: TYPES.Int, VarChar: Correo},
+        {name: 'Web', type: TYPES.Int, VarChar: Web},
+        {name: 'DescripcionProveedor', type: TYPES.VarChar, value: DescripcionProveedor},
+    ];
+    return execQuery.execWriteCommand(query, parameters)
+};
+
+const deleteProveedor = (IDProveedor) => {
+    const query = `
+    DELETE FROM [dbo].[Proveedores]
+    WHERE IDProveedor = @IDproveedor
+    `
+    const parameters = [
+        {name: 'IDProveedor', type: TYPES.Int, value: IDProveedor}
+    ];
+    return execQuery.execWriteCommand(query, parameters);
+};
+
+const allProveedor = () => {
+    const query = `
+    SELECT * FROM [dbo].[Proveedores]
+    `;
+    return execQuery.execReadCommand(query);
+}
